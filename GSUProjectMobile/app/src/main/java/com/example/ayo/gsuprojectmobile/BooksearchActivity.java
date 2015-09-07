19cc99b7c9b50060a5f67800963a9c3d0af67446
ayo <faforeayomitunde@yahoo.com>
2015-09-07 00:19:36 -0700

    added new activity

--- GSUProjectMobile/app/src/main/java/com/example/ayo/gsuprojectmobile/BooksearchActivity.java
+++ GSUProjectMobile/app/src/main/java/com/example/ayo/gsuprojectmobile/BooksearchActivity.java
@@ -1,37 +1,24 @@
 package com.example.ayo.gsuprojectmobile;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class Home extends AppCompatActivity {
+public class BooksearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        Button Buy = (Button) findViewById(R.id.btnBuy);
-        Buy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Home.this, BooksearchActivity.class));
-            }
-        });
+        setContentView(R.layout.activity_booksearch);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_booksearch, menu);
         return true;
     }
-//    public void searchBooks(View view){
-//        Intent intent = new Intent(this, BooksearchActivity.class);
-//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
